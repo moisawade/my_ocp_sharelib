967a7679e54797ea9b369b24d5a5f354588b34f3
moisawade <38717896+moisawade@users.noreply.github.com>
2025-04-02 13:49:23 +0000

    Update OpenShiftHelper.groovy

--- src/com/aviro/OpenShiftHelper.groovy
+++ src/com/aviro/OpenShiftHelper.groovy
@@ -1,1 +1,17 @@
+package com.aviro
+
+class OpenShiftHelper {
+    static void login(script, String token, String server) {
+        script.sh "oc login --token=${token} --server=${server}"
+    }
+
+    static void deploy(script, String project, String image, String appName) {
+        script.sh "oc project ${project}"
+        script.sh "oc set image deployment/${appName} ${appName}=${image} --namespace=${project}"
+    }
+
+    static boolean checkDeployment(script, String project, String appName) {
+        return script.sh(script: "oc rollout status deployment/${appName} --namespace=${project}", returnStatus: true) == 0
+    }
+}
 
