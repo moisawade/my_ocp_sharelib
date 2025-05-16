3923a924e360d6cb8c207f3951845a71bf57b96b
Moussa WADE [SNT DSI/DAC/CoE] <Moussa.WADE@orange-sonatel.com>
2025-05-16 16:32:12 +0000

    update

--- src/com/aviro/OpenShiftHelper.groovy
+++ src/com/aviro/OpenShiftHelper.groovy
@@ -10,11 +10,11 @@ class OpenShiftHelper {
     //     script.sh "oc set image deployment/${appName} ${appName}=${image} --namespace=${project}"
     // }
 
-    // static void deploy(script, String project, String image, String appName) {
-    //     script.sh "oc project ${project}"
-    //     script.sh "oc new-app -name=appName ${appName}=${image} --namespace=${project}"
-    //     //script.sh "oc set image deployment/${deploymentName} ${appName}=${image} --namespace=${project}"
-    // }
+    static void deploy(script, String project, String image, String appName) {
+        script.sh "oc project ${project}"
+        script.sh "oc new-app -name=appName ${appName}=${image} --namespace=${project}"
+        //script.sh "oc set image deployment/${deploymentName} ${appName}=${image} --namespace=${project}"
+    }
 
     // static boolean checkDeployment(script, String project, String appName) {
     //     return script.sh(script: "oc rollout status deployment/${appName} --namespace=${project}", returnStatus: true) == 0
