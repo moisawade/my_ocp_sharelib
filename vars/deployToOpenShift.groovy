f8e64a9792f83af6ffebf5347361e17a1a32a519
Moussa WADE [SNT DSI/DAC/CoE] <Moussa.WADE@orange-sonatel.com>
2025-05-16 16:15:36 +0000

    update oc create-new

--- vars/deployToOpenShift.groovy
+++ vars/deployToOpenShift.groovy
@@ -5,11 +5,21 @@ def call(String project, String image, String appName, String ocToken, String oc
         // Connexion à OpenShift
         sh "oc login --token=${ocToken} --server=${ocServer}"
 
+        if (status != 0) {
+            error " Échec du déploiement sur OpenShift !"
+        } else {
+            echo "Déploiement réussi !"
+        }
         // Sélection du projet OpenShift
         sh "oc project ${project}"
-
+        
         //Création du deploiement
-        sh "oc new-app -name=appName ${appName}=${image} --namespace=${project}"
+        def status = sh(script: sh "oc new-app -name=appName ${appName}=${image} --namespace=${project}", returnStatus: true
+        if (status != 0) {
+            error " Already exists on OpenShift !"
+        } else {
+            echo "Deployment successful !"
+        }
 
         // Mise à jour de l'image dans le déploiement
         //sh "oc set image deployment/${ocDeployment} ${appName}=${image} --namespace=${project}"
