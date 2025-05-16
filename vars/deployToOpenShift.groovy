31553df7e5150195a6fa792ffd4549cc5950b339
Moussa WADE [SNT DSI/DAC/CoE] <Moussa.WADE@orange-sonatel.com>
2025-05-16 16:30:29 +0000

    update

--- vars/deployToOpenShift.groovy
+++ vars/deployToOpenShift.groovy
@@ -10,7 +10,7 @@ def call(String project, String image, String appName, String ocToken, String oc
         sh "oc project ${project}"
         
         //Création du deploiement
-        sh("oc new-app -name=appName ${appName}=${image} --namespace=${project}", returnStatus: true)
+        sh "oc new-app -name=appName ${appName}=${image} --namespace=${project}"
 
         // Mise à jour de l'image dans le déploiement
         //sh "oc set image deployment/${ocDeployment} ${appName}=${image} --namespace=${project}"
